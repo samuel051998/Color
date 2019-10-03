a30254e5b83951d65429f12f51d5552f208ed83b
Samuele Giaccone <GiaGia@MacBook-Pro-di-patrizia-di-giovanni.local>
2019-10-03 16:00:07 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let colors = [
+	{
+		id: 1,
+		name: "red",
+		rgb: "255,0,0"
+	},
+	{
+		id: 2,
+		name: "green",
+		rgb: "0,255,0"
+	},
+	{
+		id: 3,
+		name: "blue",
+		rgb: "0,0,255"
+	}
+]
+
+let color = {};
+
+
+
